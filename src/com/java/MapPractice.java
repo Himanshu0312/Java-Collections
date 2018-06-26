9b51d236fd880813d13569b6312bade3bc0456c4
Himanshu0312 <20499582+Himanshu0312@users.noreply.github.com>
2018-06-26 16:12:56 +0530

    Adding MapExamples

---
+++ src/com/java/MapPractice.java
@@ -0,0 +1,23 @@
+package com.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapPractice {
+	public static void main(String[] args) {
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(0, "Amit");
+		map.put(1, "Vijay");
+		map.put(2, "Karan");
+		map.put(3, "Raman");
+		map.put(4, "Jai");
+		map.put(5, "Rahul");
+		System.out.println("The Map elements:");
+		
+		for (Map.Entry mEntry : map.entrySet()) {
+			System.out.println(mEntry.getKey()+" :: "+mEntry.getValue());
+		}
+		
+		//Deleted some bad comment lines
+	}
+}
